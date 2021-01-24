1c122a9d89c31152dfaad534d19e364682b4dbaa
maltsev-00 <47332063+maltsev-00@users.noreply.github.com>
2021-01-24 19:44:10 +0300

    fix-bags

--- src/main/java/com/kafka/producer/constants/Constants.java
+++ src/main/java/com/kafka/producer/constants/Constants.java
@@ -5,4 +5,5 @@ public class Constants {
     public static final String BOOTSTRAP_SERVERS = "localhost:9092";
     public static final String CLIENT_ID_CONFIG = "trans-string-consumer-egen-new";
     public static final String TOPIC = "test";
+
 }
--- src/main/java/com/kafka/producer/controller/BookController.java
+++ src/main/java/com/kafka/producer/controller/BookController.java
@@ -2,6 +2,7 @@ package com.kafka.producer.controller;
 
 import com.kafka.producer.model.Book;
 import com.kafka.producer.service.ProducerService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,15 +14,10 @@ import javax.validation.Valid;
 
 @RestController
 @RequestMapping("kafka")
+@RequiredArgsConstructor
 public class BookController {
 
     private final ProducerService producerService;
-
-    @Autowired
-    public BookController(ProducerService producerService) {
-        this.producerService = producerService;
-    }
-
 
     @PostMapping
     public Mono<Book> addNewBook(@Valid @RequestBody Book book){
